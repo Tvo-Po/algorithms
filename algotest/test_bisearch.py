dbff3912fb68a5d9849c5286cd8acf047bc88c69
Gleb <maxwellgleb@gmail.com>
2021-10-11 09:31:05 +0300

    Add test of operation count for binary search.

--- algotest/test_bisearch.py
+++ algotest/test_bisearch.py
@@ -1,32 +1,39 @@
 import unittest
+from math import ceil, log2
 
 from algo.bisearch import binary_search
 
 
 class TestBinarySearch(unittest.TestCase):
 
+    TEST_ARRAY = ['apple', 'go', 'grass', 'loser', 'newbie',
+                  'opponent', 'protect', 'punishment', 'test',
+                  'unicorn', 'unit', 'useless', 'well', 'winner',
+                  'work', 'zoo']
+
+    TEST_TUPLE = ('a', 'and', 'anywhere', 'diversify', 'do',
+                  'errors', 'free', 'goodbye', 'grammatical',
+                  'language', 'level-up', 'say', 'that', 'to',
+                  'tool', 'works', 'writing', 'you', 'your')
+
+    SEARCH_INDEX_FOR_TUPLE = 17
+    SEARCH_WORD_FOR_TUPLE = TEST_TUPLE[SEARCH_INDEX_FOR_TUPLE]
+    SEARCH_INDEX_FOR_ARRAY = 4
+    SEARCH_WORD_FOR_ARRAY = TEST_ARRAY[SEARCH_INDEX_FOR_ARRAY]
+    ARRAY_LENGTH = len(TEST_ARRAY)
+
     def test_with_array(self):
-
-        test_array = ['apple', 'go', 'grass', 'loser', 'newbie',
-                      'opponent', 'protect', 'punishment', 'test',
-                      'unicorn', 'unit', 'useless', 'well', 'winner',
-                      'work', 'zoo']
-
-        search_word = 'opponent'
-        search_word_index = 5
-
-        result = binary_search(test_array, search_word)
-        self.assertEqual(search_word_index, result)
+        result = binary_search(self.TEST_ARRAY, self.SEARCH_WORD_FOR_ARRAY)
+        self.assertEqual(self.SEARCH_INDEX_FOR_ARRAY, result)
 
     def test_with_tuple(self):
+        result = binary_search(self.TEST_TUPLE, self.SEARCH_WORD_FOR_TUPLE)
+        self.assertEqual(self.SEARCH_INDEX_FOR_TUPLE, result)
 
-        test_tuple = ('a', 'and', 'anywhere', 'diversify', 'do',
-                      'errors', 'free', 'goodbye', 'grammatical',
-                      'language', 'level-up', 'say', 'that', 'to',
-                      'tool', 'works', 'writing', 'you', 'your')
+    def test_operations_interval(self):
+        binary_sort_max_operations = ceil(log2(self.ARRAY_LENGTH))
+        result_array, amount_of_operations = binary_search(self.TEST_ARRAY, self.SEARCH_WORD_FOR_ARRAY,
+                                                           is_number_of_operations_needed=True)
+        self.assertEqual(result_array, self.SEARCH_INDEX_FOR_ARRAY)
+        self.assertLessEqual(amount_of_operations, binary_sort_max_operations)
 
-        search_word = 'writing'
-        search_word_index = 16
-
-        result = binary_search(test_tuple, search_word)
-        self.assertEqual(search_word_index, result)
